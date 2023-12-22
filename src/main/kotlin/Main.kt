dc981c96abac8ac69b832dbc857132ad53ad731b
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-12-22 17:02:01 +0300

    fiddle around with ui

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -14,27 +24,62 @@ import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 @Composable
+fun MyButton(text: String, icon: ImageVector) {
+  Button(
+    onClick = { /* Handle button click */ },
+    modifier = Modifier
+      .height(48.dp)
+  ) {
+    Icon(imageVector = icon, contentDescription = null)
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(text = text)
+  }
+}
+
+@Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+  MaterialTheme {
+    Box(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .align(Alignment.TopCenter),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+      ) {
+        Text(
+          text = "TODO List App",
+          style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+          color = Color.Black
+        )
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop 2!"
-        }) {
-            Text(text)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+          modifier = Modifier.fillMaxWidth(),
+          horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+          MyButton(text = "Button 1", icon = Icons.Default.Check)
+          MyButton(text = "Button 2", icon = Icons.Default.Close)
         }
+      }
     }
+  }
 }
 
 fun main() = application {
-    val state = WindowState(size = DpSize(300.dp, 300.dp))
+  val state = WindowState(size = DpSize(600.dp, 1200.dp))
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        resizable = false,
-        state = state
-    ) {
-        App()
-    }
+  Window(
+    onCloseRequest = ::exitApplication,
+    resizable = false,
+    state = state
+  ) {
+    App()
+  }
 }
